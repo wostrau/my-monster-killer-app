170f25bd34f93842c55eed94e697b6302a254b8d
wostrau <a.ostrovtsov@gmail.com>
2023-08-31 13:38:57 +0200

    added strong attack functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,6 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
+const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -6,9 +8,34 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-const attackHandler = () => {
-    const damage = dealMonsterDamage(ATTACK_VALUE);
+const attackMonster = (mode) => {
+    let maxDamage;
+    switch (mode) {
+        case 'ATTACK':
+            maxDamage = ATTACK_VALUE;
+            break;
+        case 'STRONG_ATTACK':
+            maxDamage = STRONG_ATTACK_VALUE;
+            break;
+    }
+
+    const damage = dealMonsterDamage(maxDamage);
     currentMonsterHealth -= damage;
+
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert('You won!');
+    }
+
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= playerDamage;
+
+    if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert('You lost!');
+    }
 };
 
+const attackHandler = () => attackMonster('ATTACK');
+const strongAttackHandler = () => attackMonster('STRONG_ATTACK');
+
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
